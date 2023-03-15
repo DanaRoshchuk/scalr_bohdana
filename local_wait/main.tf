e402ed591834525c266ef9b18a0d9e418641afad
DanaRoshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2023-03-15 12:39:58 +0200

    Update main.tf

--- local_wait/main.tf
+++ local_wait/main.tf
@@ -8,7 +8,7 @@ variable "sleep_time" {
 }
 
 resource "random_integer" "timeout" {
-  min = 32
+  min = 30
   max = 200
 
   keepers = {
@@ -28,7 +28,7 @@ resource "null_resource" "env_vars" {
 }
 
 output "very_long" {
-  value = "Qwerty. Lorem dfipsusdm dolor sit ameывt, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"  
+  value = "Webhook. Lorem dfipsusdm dolor sit ameывt, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"  
 }
 
 output "senc_out" {
