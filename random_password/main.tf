56859c2a97ebe0a60ade1f0a84ca64c1ff163603
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2023-11-08 19:41:46 +0200

    Update main.tf

--- random_password/main.tf
+++ random_password/main.tf
@@ -1,5 +1,14 @@
 resource "random_password" "password" {
+  count = 
   length           = 16
   special          = true
   override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+variables "pass_count"{
+type = string
+default = "2"
+}
+
+output "password_out" {
+  value = random_password.password[*].id
 }
