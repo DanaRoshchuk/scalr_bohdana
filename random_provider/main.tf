e2cc01adc293c776cbe44ca4adc5e2e2bee3b9dd
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-01-30 12:38:41 +0200

    Update main.tf

--- random_provider/main.tf
+++ random_provider/main.tf
@@ -6,27 +6,40 @@ terraform {
     }
   }
 }
+
 variable my_count {
   default = 600
 }
+
 resource "random_string" "random_string" {
-    count = 10
+
+    count = var.my_count
+
     length  = 4
     special = false
     upper   = true
 }
+
+
 data "vault_generic_secret" "default_terraform_version" {
    path = "kv/tfe/repl"
 }
+
+
+
 variable "vault_address" {
   default = "https://vault.infosec.ic1.statefarm:8200"
 }
+
 variable "vault_namespace" {
   default = "PCAT"
 }
+
+
 provider "vault" {
-    address   = 1212
-    namespace = 1212
+    address   = var.vault_address
+    namespace = var.vault_namespace
+
     skip_child_token = true
     token = "dummy"
 }
