f957c902fc018d8d7a424a87b082ca52e7fee225
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-02-23 15:33:01 +0200

    Update main.tf

--- referencing_to_modules/main.tf
+++ referencing_to_modules/main.tf
@@ -23,9 +23,6 @@ module "main5" {
 	source  = "gitt.dana-failed.testenv.scalr.dev/acc-v0o90lcqoc81eetg5/main/huge"
 	version = "4.4.4"
 }
-module "main6" {
-	source  = "gitt.dana-failed.testenv.scalr.dev/acc-v0o90lcqoc81eetg5/main/huge"
-	version = "4.4.4"
-}
+
 
 
