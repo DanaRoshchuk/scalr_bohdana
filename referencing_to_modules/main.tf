54c67826125aba6131826a0e8432b1eb537a2269
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-02-23 13:22:12 +0200

    Create main.tf

---
+++ referencing_to_modules/main.tf
@@ -0,0 +1,20 @@
+module "main" {
+	source  = "gitt.dana-failed.testenv.scalr.dev/acc-v0o90lcqoc81eetg5/main/huge"
+	version = "1.1.1"
+}
+module "main" {
+	source  = "gitt.dana-failed.testenv.scalr.dev/acc-v0o90lcqoc81eetg5/main/huge"
+	version = "1.1.1"
+}
+module "main" {
+	source  = "gitt.dana-failed.testenv.scalr.dev/acc-v0o90lcqoc81eetg5/main/huge"
+	version = "1.1.1"
+}
+module "main" {
+	source  = "gitt.dana-failed.testenv.scalr.dev/acc-v0o90lcqoc81eetg5/main/huge"
+	version = "1.1.1"
+}
+module "main" {
+	source  = "gitt.dana-failed.testenv.scalr.dev/acc-v0o90lcqoc81eetg5/main/huge"
+	version = "1.1.1"
+}
