c37cae546cb885026bee0e1574148a6188fa7777
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-06-04 16:44:05 +0300

    Create main.tf

---
+++ invalid_config/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_version = ">= 1.2.0"
+}
+
+variable "run_id" {
+  #default = "run_id_11"
+}
+
+variable "sleep_time" {
+  default = 1000
+}
+
+resource "random_integer" "timeout" {
+  min = 50
+  max = 100
+
+  keepers = {
+    run_id = var.run_id
+  }
+}
+
+
+
+resource "null_resource" "env_vars" {
+ count = 1
+  triggers = {
+    run_id = var.run_id
+  }
+  provisioner "local-exec" {
+    command = "env"
+  }
+}
+
+output "very_long" {
+  value = "Webhook. Lorem dfipsusdm dolor sit ameывt, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"  
+}
+
+output "senc_out" {
+  value = "secret data"
+  description = "my sensitive output"
+  sensitive = true
+}
+
+output "output_run_id" {
+  value = var.run_id
