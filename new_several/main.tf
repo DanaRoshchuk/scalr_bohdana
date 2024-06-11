f5796ce423f579a7e45fe30b1101e55d499418c6
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-06-11 17:27:54 +0300

    Create main.tf

---
+++ new_several/main.tf
@@ -0,0 +1,20 @@
+resource "random_integer" "ran" {
+  count = var.quantity
+  min = 100
+  max = 400
+  keepers = {
+    run_id = var.run_id
+  }
+}
+resource "random_pet" "pet" {
+ prefix = var.prefix
+ separator = "."
+ length = 1
+ }
+variable "quantity" {
+	type = string
+}
+variable "prefix" {
+	type = string
+}
+variable "run_id" {}
