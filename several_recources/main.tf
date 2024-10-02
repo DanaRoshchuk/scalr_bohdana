08ab1a277e6331719e5e52d28b34701400b3604c
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-10-02 16:58:04 +0300

    Update main.tf

--- several_recources/main.tf
+++ several_recources/main.tf
@@ -28,7 +28,7 @@ resource "random_pet" "pet" {
 output "sensitive1" {
     value     = "<form><a href=\"javascript:alert(1)\">X</a></form>"
 }
-/*data "terraform_remote_state" "for_output" {
+data "terraform_remote_state" "for_output" {
   backend = "remote"
 
   config = {
@@ -42,7 +42,7 @@ output "sensitive1" {
 
 output "several" {
     value     = data.terraform_remote_state.for_output.outputs.sensitive1
-}*/
+}
 
 
 
