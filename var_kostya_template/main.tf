7e2f2c41b1e9edc531a266c5f92f1e61a008dc4d
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-10-03 11:58:34 +0300

    Update main.tf

--- var_kostya_template/main.tf
+++ var_kostya_template/main.tf
@@ -82,6 +82,46 @@ output "senc_out" {
   sensitive = false
 }
 
+
+resource "random_uuid" "test" {
+count = 11
+}
+output "uuid_out" {
+  value = random_uuid.test[*].result
+sensitive = false
+}
+variable "run_id" {
+  default = "run_id_3"
+}
+
+output "output_run_id" {
+  value = var.run_id
+}
+
+resource "random_password" "password" {
+  count = var.pass_count
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+variable "pass_count"{
+type = string
+default = "5"
+}
+
+output "password_out" {
+  value = random_password.password[*].result
+sensitive = true
+}
+
+
+
+
+
+
+
+
+
 /*data "terraform_remote_state" "for_output" {
   backend = "remote"
 
