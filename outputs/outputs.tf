c75370f391741bad0c3a54642efa74c5debb6ac4
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-10-03 13:11:48 +0300

    Update outputs.tf

--- outputs/outputs.tf
+++ outputs/outputs.tf
@@ -560,3 +560,23 @@ output "alj" {
   value     = "secret"
   sensitive = true
 }
+
+
+output "note" {
+  value     = "<<-EOT
+        Web:
+          URL                   : https://dana-outputs.testenv.scalr.dev
+          Username              : admin
+          Password              : zPXrS7XmbTGESfvU2wG3
+        Shell:
+          kubectl -n dana-outputs exec -it scalr-server-0 -c scalr -- bash
+        Port-forwarding:
+          kubectl -n dana-outputs port-forward scalr-server-0 8022:22
+        UI Watcher:
+          ssh -v testenv -N -R 0.0.0.0:3000:127.0.0.1:3000
+          npx next ui/next
+        Documentation:
+          https://github.com/Scalr/terraform-google-infra/tree/master/modules/terraform-google-preview-saas#user-guide
+    EOT"
+  sensitive = false
+}
