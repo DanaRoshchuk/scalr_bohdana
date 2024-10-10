05a25d1f5ad694ace2df3958484df787585feb13
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-10-10 11:53:16 +0300

    Update main.tf

--- outputs/outputs_obj_array/main.tf
+++ outputs/outputs_obj_array/main.tf
@@ -11,3 +11,28 @@ output "uuid_obj1" {
 output "uuid_obj2" {
   value = { key0: random_uuid.test[0].result, key1: random_uuid.test[1].result, key2: "lorem", key3: { key0: random_uuid.test[0].result, key1: random_uuid.test[1].result, key2: "lorem" } }
 }
+
+
+resource "random_uuid" "test2" {
+count = 5
+}
+output "uuid_out" {
+  value = random_uuid.test2[*].result
+sensitive = false
+}
+
+resource "random_password" "password" {
+  count = var.pass_count
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+variable "pass_count"{
+type = string
+default = "5"
+}
+
+output "password_out" {
+  value = random_password.password[*].result
+sensitive = true
+}
