4749b3d954100ba5520dde199bb17d52158dde30
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-10-24 12:16:59 +0300

    Update main.tf

--- required_version/main.tf
+++ required_version/main.tf
@@ -29,3 +29,19 @@ resource "random_pet" "pet" {
 output "sensitive1" {
     value     = "<form><a href=\"javascript:alert(1)\">X</a></form>"
 }
+
+variable "quantity" {
+  type    = number
+}
+
+variable "run_id" {
+  type = string
+  default = ""
+}
+
+variable "prefix" {
+  type    = string
+}
+variable "strings" {
+ type = list(string)
+}
