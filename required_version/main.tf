293bf2982bf2ab728d0a83f7d3c3c7cbdd17f0ea
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-10-24 12:08:07 +0300

    Create main.tf

---
+++ required_version/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version > "1.5.0"
+}
+
+resource "random_integer" "ran" {
+  count = var.quantity
+  min = 100
+  max = 400
+  keepers = {
+    run_id = var.run_id
+  }
+}
+resource "random_pet" "pet" {
+ prefix = var.prefix
+ separator = "."
+ length = 1
+ }
+ resource "null_resource" "env_vars" {
+  #count = 5
+  provisioner "local-exec" {
+    command = "echo $ENV"
+    environment = {
+      ENV = "Hello World!"
+    }
+ }
+}
+
+
+output "sensitive1" {
+    value     = "<form><a href=\"javascript:alert(1)\">X</a></form>"
+}
