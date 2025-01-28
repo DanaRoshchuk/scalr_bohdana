81544ce4d4f6a30ee7c20a497a7ab9867874a9e1
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2025-01-28 12:30:42 +0200

    Update main.tf

--- ssh_key/main.tf
+++ ssh_key/main.tf
@@ -1,4 +1,4 @@
 module "git" {
-    source  = "git::ssh://git@github.com/RoshchukAccount/gittest.git"
+    source  = "git::ssh://git@github.com/RoshchukAccount/terraform-gittest-null.git"
     }
     
