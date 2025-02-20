ef4b44deae1f85c491757aa14694e6ed433b92e4
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2025-02-20 16:06:57 +0200

    Update main.tf

--- outputs/main.tf
+++ outputs/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    scalr = {
+      source  = "registry.scalr.io/scalr/scalr"
+      version = "1.8.0"
+    }
+  }
+}
 resource "random_password" "password" {
   count = var.pass_count
   length           = 16
