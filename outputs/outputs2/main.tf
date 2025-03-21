cdd6138997a7dc15b9e0ce74db044b35a884746a
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2025-03-21 12:04:18 +0200

    Create main.tf

---
+++ outputs/outputs2/main.tf
@@ -0,0 +1,60 @@
+resource "random_password" "password" {
+  count = var.pass_count
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+variable "pass_count"{
+type = string
+default = "5"
+}
+output "password_out" {
+  value = random_password.password[*].result
+sensitive = true
+}
+output "alj" {
+  value     = "secret"
+  sensitive = true
+}
+
+
+output "note" {
+  value     = <<-EOT
+        Web:
+          URL                   : https://dana-outputs.testenv.scalr.dev
+          Username              : admin-super-star
+          Password              : zPXrS7XmbTGESfvU2wG3
+        Shell:
+          kubectl -n dana-outputs exec -it scalr-server-0 -c scalr -- bash
+        Port-forwarding:
+          kubectl -n dana-outputs port-forward scalr-server-0 8022:22
+        UI Watcher:
+          ssh -v testenv -N -R 0.0.0.0:3000:127.0.0.1:3000
+          npx next ui/next
+        Documentation:
+          https://github.com/Scalr/terraform-google-infra/tree/master/modules/terraform-google-preview-saas#user-guide
+    EOT
+
+  sensitive = false
+}
+
+output "just_added" {
+  value     = "secret"
+  sensitive = false
+  description = "key moving focus. Alternatively, use esc then tab to move to the next interactive element on the page."
+}
+output "multi_line" { # create sensitive shell variable with the same value as output (and its name)
+  value = {
+    "product": {
+        "id": "12345",
+        "name": "Wireless Mouse",
+        "price": 25.99,
+        "in_stock": true,
+        "specifications": {
+        "color": "black",
+        "connectivity": "wireless",
+        "battery_life": "12 months"
+        }
+    }
+  }
+}
