b86617686bf543d80fe52e25c6be85e29eed3af4
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2025-03-28 12:39:02 +0200

    Create main.tf

---
+++ data_for_folder_module_inside/main.tf
@@ -0,0 +1,11 @@
+resource "null_resource" "long_attribute_names" {
+  triggers = {
+    time = timestamp()
+    "long.attribute-name-approx-fourty-one-symbol" = var.trigger-value
+  }
+}
+
+variable "trigger-value" {
+  type = string
+default = ""
+}
