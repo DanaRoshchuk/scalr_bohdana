134694eef9d45a1981681af631d8f4f68aa24b98
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2025-05-02 12:30:48 +0300

    Create main.tf

---
+++ template_opentofu-1.9.0_ for_each/main.tf
@@ -0,0 +1,62 @@
+
+
+terraform{
+backend "remote" {
+  hostname = "amykh4223.main.scalr.dev"
+  organization = "s3"
+workspaces {
+      name = "clitest"
+    }
+}
+}
+resource "random_pet" "pet" {
+  keepers = {
+    # Generate a new pet name each time
+    timestamp = timestamp()
+  }
+}
+
+output "very_long" {
+  value = "Lorem dfipsusdm dolor sit am"  
+}
+
+output "senc_out" {
+  value = "secret data"
+  description = "my sensitive output"
+  sensitive = true
+}
+
+output "env" {
+  value = var.environment
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+
+locals {
+  config = yamldecode(file("${path.module}/${var.environment}.yaml"))
+  regions = local.config.regions
+}
+
+provider "aws" {
+  alias = "by_region"
+  region = each.key
+}
+
+resource "aws_s3_bucket" "example" {
+  count = 1
+  bucket = "user-tf-test-s3-bucket-${count.index}"
+
+  tags = {
+    Name        = "My bucket-upd"
+    Environment = "var.environment"
+  }
+}
