81a559e37c21d0f2e906c21098483b1887d0c665
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2025-05-09 15:03:53 +0300

    Update main.tf

--- auto_null_provider/main.tf
+++ auto_null_provider/main.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     null = {
       source = "hashicorp/null"
-      version = "3.2.1"
+      version = "3.2.0"
     }
   }
 }
 
-resource "null_resource" "single" {
+resource "null_resource" "single2" {
 count= 1
   triggers= {
     time= timestamp()
