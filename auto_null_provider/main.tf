b9a0170a45b73179decbf0ef9910915b87d9eeea
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2025-05-09 14:32:31 +0300

    Create main.tf

---
+++ auto_null_provider/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.0"
+    }
+  }
+}
+
+resource "null_resource" "resource3" {
+  provisioner "local-exec" {
+    command = "echo $ENV"
+    environment = {
+      ENV = "Hello World!"
+    }
+  }
+}
