155a6111842309a3510212b1571691e7f6a27c6b
danaroshchuk <bohdana.smal@gmail.com>
2025-07-18 16:52:33 +0300

    Update main.tf

--- outputs/outputs3/main.tf
+++ outputs/outputs3/main.tf
@@ -1,5 +1,5 @@
 
-provider "aws" {
+/*provider "aws" {
  region = var.region
 }
 data "aws_ami" "ubuntu" {
@@ -29,4 +29,17 @@ resource "aws_instance" "ubuntu" {
     create = "9m"
     delete = "15m"
   }
+}*/
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = sensitive("name")
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"]
 }
