f84b8523e1b3f1bde3bf0835ccdb608f4ab8453b
danaroshchuk <bohdana.smal@gmail.com>
2025-07-18 15:36:02 +0300

    Update main.tf

--- outputs/outputs3/main.tf
+++ outputs/outputs3/main.tf
@@ -1,19 +1,36 @@
-resource "random_password" "password" {
-  count = var.pass_count
-  length           = 16
-  special          = true
-  override_special = "!#$%&*()-_=+[]{}<>:?"
-}
 variable "pass_count"{
-type = string
-default = "5"
+  type = string
+  default = "5"
+  sensitive=true
 }
-
-output "password_out" {
-  value = random_password.password[*].result
-sensitive = true
+provider "aws" {
+ region = var.region
 }
-output "admin_token" {
-value = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjU0NDAyNjUsImlzcyI6InNjY"
-description = ""
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = sensitive("name")
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"]
+}
+resource "aws_instance" "ubuntu" {
+  count = 100
+   provisioner "local-exec" {
+    command = "env"
+  }
+  #ami                         = var.win_amiwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwin_amiwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwin_amiwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwin_amiwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwin_amiwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+  ami           = data.aws_ami.ubuntu.id
+  #instance_type = var.instance_type1
+ instance_type = sensitive("t2.nano")
+  subnet_id     = var.subnet
+  tags          = merge({ "Name" = format("k.kotov-test -> %s -> %s", substr("🤔🤷", 0, 1), data.aws_ami.ubuntu.name) }, var.tags)
+  timeouts {
+    create = "9m"
+    delete = "15m"
+  }
 }
